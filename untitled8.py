e90a1bf87079d1326747121b84d89532d37dc3d7
unknown <ad.chomicki@gmail.com>
2020-12-19 11:57:07 +0100

    do str. 92

---
+++ untitled8.py
@@ -0,0 +1,221 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from zlib import crc32
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedShuffleSplit
+from pandas.plotting import scatter_matrix
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OrdinalEncoder
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.compose import ColumnTransformer
+from sklearn.linear_model import LinearRegression
+
+housing = pd.read_csv(r"C:/Users/Adam/Documents/dane_ksiazka/uczem2/zestawy_danych\mieszkania/housing.csv")
+
+housing.head()
+
+housing.info()
+
+housing.isnull().sum()
+
+# wyswietla poszczególne kategorie z tablicy
+housing["ocean_proximity"].value_counts()
+
+housing.describe()
+
+# PODZIAŁ 2 - POPRAWNE
+train_dataset = housing.sample(frac=0.8, random_state=0)
+test_dataset = housing.drop(train_dataset.index)
+
+
+# PODZIAŁ 1 - NIEPOPRAWNE
+X = housing.drop(['median_house_value'], axis=1)
+X = housing.drop(['ocean_proximity'], axis='columns')
+y = housing['median_house_value']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+lr = LinearRegression()
+lr.fit(X_train, y_train)
+ 
+# PODZIAŁ - KSIĄŻKI
+def split_train_test(data, test_ratio):
+    shuffled_indices = np.random.permutation(len(data))
+    test_set_size = int(len(data) * test_ratio)
+    test_indices = shuffled_indices[:test_set_size]
+    train_indices = shuffled_indices[test_set_size:]
+    return data.iloc[train_indices], data.iloc[test_indices]
+
+train_set, test_set = split_train_test(housing, 0.2)
+
+len(train_set)
+len(test_set)
+
+# MECHANIZM STABILNOSCI ZBIORY TESTOWEGO
+def test_set_check(identifier, test_ratio):
+    return crc32(np.int64(identifier)) & 0xffffffff < test_ratio * 2**32
+
+def split_train_test_by_id(data, test_ratio, id_column):
+    ids = data[id_column]
+    in_test_set = ids.apply(lambda id_: test_set_check(id_, test_ratio))
+    return data.loc[-in_test_set], data.loc[in_test_set]
+
+# DODANIE KOLUMNY 'INDEX'
+housing_with_id = housing.reset_index()
+train_dataset, test_dataset = split_train_test_by_id(housing_with_id, 0.2, "index")
+
+# TEGO NIE POTRZEBUJE
+housing_with_id["id"] = housing["longitude"] * 1000 +housing["latitude"]
+train_dataset, test_dataset = split_train_test_by_id(housing_with_id, 0.2, "id")
+
+# PO CO KOLEJNE DZIELENIE ZBIORU NA LOSOWE PODZBIORY?
+train_dataset, test_dataset = train_test_split(housing, test_size=0.2, random_state=42)
+
+housing["income_cat"] = pd.cut(housing["median_income"],
+                               bins=[0, 1.5, 3.0, 4.5, 6., np.inf],
+                               labels=[1, 2, 3, 4, 5])
+
+housing["income_cat"].hist()
+
+# PRÓBKOWANIE WARSTWOWE NA PODSTAWIE KATEGORII DOCHODÓW
+split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+
+for train_index, test_index in split.split(housing, housing["income_cat"]):
+    strat_train_set = housing.loc[train_index]
+    strat_test_set = housing.loc[test_index]
+
+strat_test_set["income_cat"].value_counts() / len(strat_test_set)
+
+for set_ in (strat_train_set, strat_test_set):
+    set_.drop("income_cat", axis=1, inplace=True)
+
+# TO MIAŁA BYĆ KOPIA, ZA ZBIOR JEST ZMNIEJSZY
+housing_copy = strat_train_set.copy()
+
+housing.plot(kind="scatter", x="longitude", y="latitude")
+housing.plot(kind="scatter", x="longitude", y="latitude", alpha=0.1)
+housing.plot(kind="scatter", x="longitude", y="latitude", alpha=0.4,
+             s=housing["population"]/100, label="Populacja", figsize=(10,7),
+             c="median_house_value", cmap=plt.get_cmap("jet"), colorbar=True,
+             )
+plt.legend()
+
+corr_matrix = housing.corr()
+
+corr_matrix["median_house_value"].sort_values(ascending=False)
+
+attributes = ["median_house_value", "median_income", "total_rooms",
+              "housing_median_age"]
+
+scatter_matrix(housing[attributes], figsize=(12,8))
+
+housing.plot(kind="scatter", x="median_income", y="median_house_value", alpha=0.1)
+
+housing["Pokoje_na_rodzinę"] = housing["total_rooms"]/housing["households"]
+housing["Sypialnie_na_rodzinę"] = housing["total_bedrooms"]/housing["total_rooms"]
+housing["Populacja_na_rodzinę"] = housing["population"]/housing["households"]
+
+corr_matrix = housing.corr()
+corr_matrix["median_house_value"].sort_values(ascending=False)
+
+housing = strat_train_set.drop("median_house_value", axis=1)
+housing_labels = strat_train_set["median_house_value"].copy()
+
+housing.dropna(subset=["total_bedrooms"])
+housing.drop("total_bedrooms", axis=1)
+median = housing["total_bedrooms"].median()
+housing["total_bedrooms"].fillna(median, inplace=True)
+
+imputer = SimpleImputer(strategy="median")
+housing_num = housing.drop("ocean_proximity", axis=1)
+imputer.fit(housing_num)
+
+imputer.statistics_
+housing_num.median().values
+
+X = imputer.transform(housing_num)
+housing_tr = pd.DataFrame(X, columns=housing_num.columns,
+                          index=housing_num.index)
+
+housing_cat = housing[["ocean_proximity"]]
+housing_cat.head(10)
+
+ordinal_encoder = OrdinalEncoder()
+housing_cat_encoded = ordinal_encoder.fit_transform(housing_cat)
+housing_cat_encoded[:10]
+
+ordinal_encoder.categories_
+
+cat_encoder = OneHotEncoder()
+housing_cat_1hot = cat_encoder.fit_transform(housing_cat)
+housing_cat_1hot
+
+housing_cat_1hot.toarray()
+
+cat_encoder.categories_
+
+rooms_ix, bedrooms_ix, population_ix, household_ix = [
+    list(housing.columns).index(col)
+    for col in ("total_rooms", "total_bedrooms", "population", "households")]
+
+class CombinedAttributesAdder(BaseEstimator, TransformerMixin):
+    def __init__(self, add_bedrooms_per_room = True): 
+        self.add_bedrooms_per_room = add_bedrooms_per_room
+    def fit(self, X, y=None):
+        return self  
+    def transform(self, X, y=None):
+        rooms_per_household = X[:, rooms_ix] / X[:, household_ix]
+        population_per_household = X[:, population_ix] / X[:, household_ix]
+        if self.add_bedrooms_per_room:
+            bedrooms_per_room = X[:, bedrooms_ix] / X[:, rooms_ix]
+            return np.c_[X, rooms_per_household, population_per_household,
+                         bedrooms_per_room]
+        else:
+            return np.c_[X, rooms_per_household, population_per_household]
+        
+attr_adder = CombinedAttributesAdder(add_bedrooms_per_room=False)
+housing_extra_attribs = attr_adder.transform(housing.values)
+
+num_pipeline = Pipeline([
+    ('imputer', SimpleImputer(strategy="median")),
+    ('attribs_adder', CombinedAttributesAdder()),
+    ('std_scaler', StandardScaler()),
+    ])
+
+num_attribs = list(housing_num)
+
+cat_attribs = ["ocean_proximity"]
+
+full_pipeline = ColumnTransformer([
+    ("num", num_pipeline, num_attribs),
+    ("cat", OneHotEncoder(), cat_attribs),
+    ])
+
+lin_reg = LinearRegression()
+lin_reg.fit(housing_prepared, housing_labels)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
